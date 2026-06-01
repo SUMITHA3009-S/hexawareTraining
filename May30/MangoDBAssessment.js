63cb7988bca1ac4ac7cff6caea84ee339a977d6a
SUMITHA3009-S <sumithasivakumar3009@gmail.com>
2026-06-01 10:38:40 +0530

    Update MangoDBAssessment.js

--- May30/MangoDBAssessment.js
+++ May30/MangoDBAssessment.js
@@ -119,19 +119,7 @@ email: null
 }
 }
 ])
-{
-  acknowledged: true,
-  insertedIds: {
-    '0': ObjectId('6a1ab583d0b388a7527fa315'),
-    '1': ObjectId('6a1ab583d0b388a7527fa316'),
-    '2': ObjectId('6a1ab583d0b388a7527fa317'),
-    '3': ObjectId('6a1ab583d0b388a7527fa318'),
-    '4': ObjectId('6a1ab583d0b388a7527fa319'),
-    '5': ObjectId('6a1ab583d0b388a7527fa31a'),
-    '6': ObjectId('6a1ab583d0b388a7527fa31b'),
-    '7': ObjectId('6a1ab583d0b388a7527fa31c')
-  }
-}
+
 db.restaurants.find()
 
 db.restaurants.find({},{name:1,city:1,cuisine:1,_id:0}) //2
@@ -150,62 +138,33 @@ db.restaurants.find({ rating: { $gte: 4.0, $lte: 4.7 } })
 
 db.restaurants.find({ avg_order_value: { $gte: 600 } })//8
 
-db.restaurants.find({
-  city: "Hyderabad",
-  delivery_available: true
-})
-
+db.restaurants.find({ city: "Hyderabad", delivery_available: true})
 
 db.restaurants.find({$or:[{city:"chennai"},{cuisine:"Indian"}]})
 
 db.restaurants.find({ delivery_available: false })
 
-db.restaurants.find({
-  city: { $in: ["Hyderabad", "Delhi", "Mumbai"] }
-})
+db.restaurants.find({  city: { $in: ["Hyderabad", "Delhi", "Mumbai"] }})
 
-db.restaurants.find({
-  cuisine: { $in: ["Indian", "Italian", "Cafe"] }
-})
+db.restaurants.find({  cuisine: { $in: ["Indian", "Italian", "Cafe"] }})
 
-db.restaurants.find({
-  city: { $nin: ["Hyderabad", "Bangalore"] }
-})
+db.restaurants.find({city: { $nin: ["Hyderabad", "Bangalore"] }})
 
-db.restaurants.find({
-  name: /^P/
-})
+db.restaurants.find({name: /^P/})
 
-db.restaurants.find({
-  name: /Point/
-})
+db.restaurants.find({name: /Point/})
 
-db.restaurants.find({
-  cuisine: /Food/
-})
+db.restaurants.find({cuisine: /Food/})
 
-db.restaurants.find({
-  "contact.phone": null
-})
+db.restaurants.find({"contact.phone": null})
 
-db.restaurants.find({
-  "contact.email": null
-})
+db.restaurants.find({"contact.email": null})
 
-db.restaurants.find({
-  $or: [
-    { "contact.phone": null },
-    { "contact.email": null }
-  ]
-})
+db.restaurants.find({$or: [{ "contact.phone": null },{ "contact.email": null }]})
 
-db.restaurants.find({
-  tags: "premium"
-})
+db.restaurants.find({tags: "premium"})
 
-db.restaurants.find({
-  tags: { $all: ["north indian", "premium"] }
-})
+db.restaurants.find({tags: { $all: ["north indian", "premium"] }})
 
 db.restaurants.find().sort({ rating: -1 })
 
@@ -215,118 +174,37 @@ db.restaurants.find().sort({ avg_order_value: 1 })
 
 db.restaurants.find().sort({ avg_order_value: -1 }).limit(2)
 
-db.restaurants.updateOne(
-  { name: "Burger Street" },
-  { $set: { rating: 4.0 } }
-)
+db.restaurants.updateOne({ name: "Burger Street" },{ $set: { rating: 4.0 } })
 
-db.restaurants.updateOne(
-  { name: "Tea Tales" },
-  { $set: { delivery_available: true } }
-)
+db.restaurants.updateOne({ name: "Tea Tales" },{ $set: { delivery_available: true } })
 
-db.restaurants.updateMany(
-  {},
-  { $set: { active: true } }
-)
+db.restaurants.updateMany({},{ $set: { active: true } })
 
-db.restaurants.updateOne(
-  { name: "Spice Hub" },
-  { $push: { tags: "popular" } }
-)
+db.restaurants.updateOne({ name: "Spice Hub" },{ $push: { tags: "popular" } })
 
-db.restaurants.updateMany(
-  {},
-  { $unset: { active: "" } }
-)
+db.restaurants.updateMany({},{ $unset: { active: "" } })
 
-db.restaurants.deleteOne({
-  restaurant_id: 6
-})
+db.restaurants.deleteOne({restaurant_id: 6})
 
-db.restaurants.deleteMany({
-  rating: { $lt: 4.0 }
-})
+db.restaurants.deleteMany({rating: { $lt: 4.0 }})
 
 db.restaurants.countDocuments()
-7
-db.restaurants.countDocuments({
-  delivery_available: true
-})
-6
+
+db.restaurants.countDocuments({delivery_available: true})
+
 db.restaurants.distinct("city")
-[ 'Bangalore', 'Chennai', 'Delhi', 'Hyderabad', 'Mumbai' ]
+
 db.restaurants.distinct("cuisine")
-[
-  'Fast Food',
-  'Healthy',
-  'Indian',
-  'Italian',
-  'Seafood',
-  'South Indian'
-]
-db.restaurants.aggregate([
-  {
-    $group: {
-      _id: "$city",
-      count: { $sum: 1 }
-    }
-  }
-])
 
-db.restaurants.aggregate([
-  {
-    $group: {
-      _id: "$cuisine",
-      count: { $sum: 1 }
-    }
-  }
-])
+db.restaurants.aggregate([ {$group: {_id: "$city",count: { $sum: 1 }}}])
 
-db.restaurants.aggregate([
-  {
-    $group: {
-      _id: "$cuisine",
-      avgRating: { $avg: "$rating" }
-    }
-  }
-])
+db.restaurants.aggregate([{$group: {_id: "$cuisine",count: { $sum: 1 }}}])
 
-db.restaurants.aggregate([
-  {
-    $group: {
-      _id: "$city",
-      avgOrderValue: { $avg: "$avg_order_value" }
-    }
-  }
-])
+db.restaurants.aggregate([{$group: {_id: "$cuisine",avgRating: { $avg: "$rating" }}}])
 
-db.restaurants.aggregate([
-  {
-    $group: {
-      _id: "$cuisine",
-      highestAvgOrderValue: { $avg: "$avg_order_value" }
-    }
-  },
-  {
-    $sort: { highestAvgOrderValue: -1 }
-  }
-])
+db.restaurants.aggregate([{$group: {_id: "$city",avgOrderValue: { $avg: "$avg_order_value" }}}])
 
-db.restaurants.aggregate([
-  {
-    $group: {
-      _id: "$cuisine",
-      count: { $sum: 1 }
-    }
-  },
-  {
-    $match: {
-      count: { $gt: 1 }
-    }
-  }
-])
-{
-  _id: 'Indian',
-  count: 2
-}
+db.restaurants.aggregate([{$group: {_id: "$cuisine",highestAvgOrderValue: { $avg: "$avg_order_value" } }},{$sort: { highestAvgOrderValue: -1 }}])
+
+db.restaurants.aggregate([{$group: {_id: "$cuisine",count: { $sum: 1 }}},{$match: {count: { $gt: 1 }}}])
+
