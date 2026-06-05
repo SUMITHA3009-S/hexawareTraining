c7185d54dd31f95ec231232f78a2c150d52ec6a1
SUMITHA3009-S <sumithasivakumar3009@gmail.com>
2026-06-05 18:02:10 +0530

    Create csv.py

---
+++ June05/csv.py
@@ -0,0 +1,57 @@
+import csv
+
+with open("employees.csv","r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+      
+# display employee names
+    for row in reader:
+        print(row[1])
+
+# count employees
+import csv
+count=0
+with open("employees.csv","r") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        count+=1
+print(count)
+----------------------------ASSESSMENT-------------------------------------------------
+# highest_salary
+highest_salary=0
+for read in reader:
+    if read["salary"] > highest_salary:
+        highest_salary = read["salary"]
+print(highest_salary)
+
+# lowest_salary
+lowest_salary=0
+for read in reader:
+    if read["salary"] < lowest_salary:
+        lowest_salary = read["salary"]
+print(lowest_salary)
+
+# average salary
+total_salary=0
+for row in reader:
+    total_salary += row["salary"]
+average_salary=total_salary/len(reader)
+print(average_salary)
+print(total_salary)
+
+# display hyderabad employees
+for row in reader:
+    if row["city"] == "Hyderabad":
+        print(row)
+      
+# display data engineering employees
+for row in reader:
+    if row["department"] == "AI Engineering":
+        print(row)
+      
+# employee earning more than 80000
+for row in reader:
+    if row["salary"]>80000:
+        print(row)
