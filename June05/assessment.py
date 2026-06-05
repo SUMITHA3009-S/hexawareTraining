363b2daa34fce61d59910bd7c220d73eb8d985eb
SUMITHA3009-S <sumithasivakumar3009@gmail.com>
2026-06-05 18:15:40 +0530

    Update assessment.py

--- June05/assessment.py
+++ June05/assessment.py
@@ -1,1 +1,37 @@
+# CSV ASSESSMENT
+# highest_salary
+highest_salary=0
+for read in reader:
+    if read["salary"] > highest_salary:
+        highest_salary = read["salary"]
+print(highest_salary)
 
+# lowest_salary
+lowest_salary=0
+for read in reader:
+    if read["salary"] < lowest_salary:
+        lowest_salary = read["salary"]
+print(lowest_salary)
+
+# average salary
+total_salary=0
+for row in reader:
+    total_salary += row["salary"]
+average_salary=total_salary/len(reader)
+print(average_salary)
+print(total_salary)
+
+# display hyderabad employees
+for row in reader:
+    if row["city"] == "Hyderabad":
+        print(row)
+      
+# display data engineering employees
+for row in reader:
+    if row["department"] == "AI Engineering":
+        print(row)
+      
+# employee earning more than 80000
+for row in reader:
+    if row["salary"]>80000:
+        print(row)
